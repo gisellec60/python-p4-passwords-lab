98c6a416a0743e6d9155be808c0ea7ba7c7d2e0e
Giselle <gisellec60@gmail.com>
2023-09-15 10:58:40 -0400

    passed

--- server/models.py
+++ server/models.py
@@ -12,8 +12,10 @@ class User(db.Model, SerializerMixin):
 
     @hybrid_property
     def password_hash(self):
-        raise Exception('Password hashes may not be viewed.')
-
+        if not self._password_hash:
+            raise Exception('Password hashes may not be viewed.')
+        return self._password_hash
+    
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(
